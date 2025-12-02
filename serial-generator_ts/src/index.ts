8410a2a0f1d16b1c044f34418448c88737e09f56
eyemask57 <eyemask.ch.m.ph@gmail.com>
2025-12-02 19:06:31 +0900

    意味なく作ったNextを削除，tsを追加．

---
+++ serial-generator_ts/src/index.ts
@@ -0,0 +1,15 @@
+const message: string = "Hello,world!";
+console.log(message);
+
+const greeting = "Hello,";
+const target = "world!";
+console.log(greeting + target);
+
+const text = greeting + target;
+console.log(text);
+
+const i = 5;
+if (i < 10) {
+    console.log("i is less than 10");
+}
+
